8275d8f1734e34f2b51d3e72bbb130a99b796193
konrad101296 <konradslimak@o2.pl>
2024-03-01 17:33:24 +0100

    Add project files.

---
+++ week 2 lesson 7 exercise nr.10/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("podaj pierwsza długość");
+int długosc1 = int.Parse(Console.ReadLine());
+Console.WriteLine("podaj druga długosc:");
+int długosc2 = int.Parse(Console.ReadLine());
+Console.WriteLine("podaj trzecia długosc");
+int długosc3 = int.Parse(Console.ReadLine());
+
+if (długosc1 >= długosc3 && długosc1 >= długosc2 && długosc3 + długosc2 > długosc1)
+{
+    Console.WriteLine("można zdudowac trojkat");
+}
+else if (długosc2 >= długosc1 && długosc2 >= długosc3 && długosc1 + długosc3 > długosc2)
+{
+    Console.WriteLine("można zdudowac trojkat");
+}
+else if (długosc3 >= długosc1 && długosc3 >= długosc2 && długosc1 + długosc2 > długosc3)
+{
+    Console.WriteLine("można zdudowac trojkat");
+}
+else
+{
+    Console.WriteLine("nie mozna zbudowac trojkata");
+}
